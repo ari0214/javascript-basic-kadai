acbd2aa6652a05919a28986793812f88b5149476
ari0214 <168341598+ari0214@users.noreply.github.com>
2024-07-20 10:52:15 +0900

    kadai?007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,33 +1,16 @@
-let num = Math.floor(Math.random()*16);
+let num = Math.floor(Math.random()*16)+1;
 
 console.log(num);
 
-if (num === 3 ){
-  console.log('3の倍数です');
-}
-
-if (num === 6 ){
-  console.log('3の倍数です');
-}
-
-if (num === 9){
-  console.log('3の倍数です');
-}
-
-if (num === 12){
-  console.log('3の倍数です');
-}
-
-if (num === 5){
-  console.log('5の倍数です');
-}
-
-if (num === 10){
-  console.log('5の倍数です');
-}
-
-if (num === 15){
+if (num % 3 === 0 && num % 5 === 0){
   console.log('3と5の倍数です');
+}
+
+else if(num % 5 === 0){
+  console.log('5の倍数です');
+}
+else if(num % 3 === 0){
+  console.log('3の倍数です');
 }
 
 else{
