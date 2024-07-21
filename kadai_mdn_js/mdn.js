b4aaab61bb6ef1001e0838549385defc3a488db9
ari0214 <168341598+ari0214@users.noreply.github.com>
2024-07-21 14:30:19 +0900

    関数をMDNで調べて使ってみよう-2

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
 const year = today.getFullYear();
-const Month = today.getMonth();
+const Month = today.getMonth()+1;
 const date = today.getDate();
 
 console.log(year + '年',Month +'月',date +'日')
